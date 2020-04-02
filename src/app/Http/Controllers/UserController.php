7042cea65d2bab6ef2a6cea3cf8e7c06d03c3392
Ignjat Jokanovic <ignjat.jokanovic.311.16@ict.edu.rs>
2020-04-02 19:08:35 +0200

    Added login with validation

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -7,25 +7,60 @@ use App\User;
 use Validator;
 use JWTAuth;
 
+
 class UserController extends Controller
 {
    
     public function store()
     {
-        // dd(request()->all());
-        $user = new User();
-        $user->name = request()->name;
-        $user->email = request()->email;
-        $user->password = request()->password;
-        if($user->save()){
-            return response()->json("Sucessfully registered", 201);
-        }else{
-            return response()->json("Something went wrong", 402);
-        }
+       $validator = Validator::make(request()->all(), [
+           'name' => 'required|alpha',
+           'email' => 'required|email',
+           'password' => 'required|alpha_num'
+       ]);
+       if($validator->fails()){
+            $errors = array();
+            foreach($validator->errors()->all() as $error)
+            {
+                array_push($errors, $error);
+            }
+            return response()->json(["messages" => $errors], 422);
+       }else{
+            $user = new User();
+            $user->name = request()->name;
+            $user->email = request()->email;
+            $user->password = bcrypt(request()->password);
+            if($user->save()){
+                return response()->json("Sucessfully registered", 201);
+            }else{
+                return response()->json("Something went wrong", 422);
+            }
+       }
+        
     }
 
     public function login(){
-
+        $credentials = request()->only(['email', 'password']);
+        $validator = Validator::make($credentials, [
+            'email' => 'required|email',
+            'password' => 'required|alpha_num'
+        ]);
+        if($validator->fails()){
+            $errors = array();
+            foreach($validator->errors()->all() as $error)
+            {
+                array_push($errors, $error);
+            }
+            return response()->json(["messages" => $errors], 422);
+        }else{
+            $token = auth()->attempt($credentials);
+            dd($token);
+            if($token){
+                return response()->json(['token' => $token], 202);
+            }else {
+                return response()->json(['messages' => ['Invalid username or password']], 401);
+            }
+        }
     }
 
     public function sendFriendRequest(){
---
+++ src/app/Http/Middleware/Jwt.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+
+class Jwt
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try 
+        {
+            $user = JWTAuth::parseToken()->authenticate();
+        } 
+        catch (Exception $e) 
+        {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException)
+            {
+                return response()->json(['messages' => ['Token is Invalid please login']], 401);
+            }
+            else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException)
+            {
+                return response()->json(['messages' => ['Session Expired please login']], 401);
+            }
+            else
+            {
+                return response()->json(['messages' => ['You must be logged in to see this page']], 401);
+            }
+        }
+        return $next($request);
+        }
+    }
+}
